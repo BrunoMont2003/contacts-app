e352804967811ac89d8a2668bb9d396f8fbee1f1
brunomont <brunoadrian.mont@gmail.com>
2022-04-23 18:56:17 -0500

    Delete contact

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+require "./db.php";
+$id = $_GET["id"];
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$statement->bindParam(":id", $id);
+$statement->execute();
+if ($statement->rowCount() == 0) {
+    http_response_code(404);
+    echo ("HTTP Error - Contact not found");
+}
+
+$statement = $conn->prepare("DELETE FROM contacts WHERE id = :id");
+$statement->bindParam(":id", $id);
+$statement->execute();
+header("Location: index.php");
